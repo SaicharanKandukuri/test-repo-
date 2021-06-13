9c74fe6e8465ccddc601d6dc3572a28476d45ecd
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-06-13 19:00:16 +0530

    GG10

--- gg.sh
+++ gg.sh
@@ -3,12 +3,4 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-apt update ; apt install cowsay -y; apt install bash
-
-if bash /ff.sh; then
-    cowsay GG
-else
-    cowsay fuck you mate
-fi
-
-neofetch
+apt update ; apt install cowsay -y; apt install bash; cowsay GG
