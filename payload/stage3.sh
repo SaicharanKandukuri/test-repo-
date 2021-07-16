d439f3273152454552e3974ae0f58c84bea5037d
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-07-16 12:28:12 +0530

    new exp

---
+++ payload/stage3.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# DE
+
+apt install xfce4 xubuntu-desktop build-essential curl tigervnc* -y
---
+++ payload/stage4.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mkdir -pv .vnc
+curl -L -o .vnc/xstartup https://raw.githubusercontent.com/RandomCoderOrg/fs-manager-hippo/main/etc/proot-env/vnc/xstartup
