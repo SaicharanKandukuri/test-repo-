066015cccc914658c8f233e8408505e93864613c
zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-07-22 07:05:26 +0530

    test 1

--- payload/stage3.sh
+++ payload/stage3.sh
@@ -2,6 +2,18 @@
 
 DEPS="git xfce4 xubuntu-desktop curl tigervnc*"
 ADEPS="imagemagick optipng inkscape"
+repos="WhiteSur-gtk-theme WhiteSur-icon-theme"
 
-apt install -y $DEPS
-apt install -y $ADEPS
+apt install -y "$DEPS"
+apt install -y "$ADEPS"
+
+if [ -d "/root" ]; then
+    cd "/root" || true
+fi
+
+for item in $repos; do
+    git clone "${item}"
+    cd "${item}" || true
+    bash install.sh
+    cd ../ || true
+done
--- payload/stage4.sh
+++ payload/stage4.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+if [ -d "/root" ]; then
+    cd "/root" || true
+fi
+
 mkdir -pv .vnc
 curl -L -o .vnc/xstartup https://raw.githubusercontent.com/RandomCoderOrg/fs-manager-hippo/main/etc/proot-env/vnc/xstartup
+chmod +x .vnc/xstartup
+cd ../ || true
