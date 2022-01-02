11d0e86cdb21d03ac3b3d95533e59f7640635551
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-01-02 12:27:15 +0530

    workdir

--- scripts/build-impish.sh
+++ scripts/build-impish.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 REPO_URL="https://github.com/RandomCoderOrg/fs-cook"
+WORKDIR="~"
 
+cd "$WORKDIR" || exit 1
 git clone $REPO_URL
-
 cd "$(basename $REPO_URL)" || exit 1 
 
 source plugins/envsetup
@@ -12,6 +13,7 @@ source plugins/colors
 export OVERRIDER_COMPRESSION_TYPE
 export ENABLE_EXIT
 export ENABLE_USER_SETUP
+
 frn="out/hirsute-raw"
 OVERRIDER_COMPRESSION_TYPE="gzip"
 ENABLE_EXIT=true
@@ -21,4 +23,3 @@ PREFIX="${frn}-arm64"
 do_debootstrap "$PREFIX" arm64
 do_compress    "$PREFIX"
 do_unmount "$PREFIX"
-
