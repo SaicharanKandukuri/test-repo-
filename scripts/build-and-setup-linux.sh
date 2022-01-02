e7282ed8ea8139af3f5a42313b5bddf93efb89ad
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-01-02 10:37:58 +0530

    less scripts

--- scripts/build-and-setup-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source plugins/envsetup
-source plugins/colors
-
-do_compress    "$PREFIX"
-do_unmount "$PREFIX"
--- scripts/build-impish.sh
+++ scripts/build-impish.sh
@@ -9,5 +9,16 @@ cd "$(basename $REPO_URL)" || exit 1
 source plugins/envsetup
 source plugins/colors
 
+export OVERRIDER_COMPRESSION_TYPE
+export ENABLE_EXIT
+export ENABLE_USER_SETUP
+frn="out/hirsute-raw"
+OVERRIDER_COMPRESSION_TYPE="gzip"
+ENABLE_EXIT=true
+ENABLE_USER_SETUP=false
+PREFIX="${frn}-arm64"
+
 do_debootstrap "$PREFIX" arm64
+do_compress    "$PREFIX"
+do_unmount "$PREFIX"
 
